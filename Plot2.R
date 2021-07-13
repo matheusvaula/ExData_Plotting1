8fa769a23397fbde11dbe2c073cad431cb6aca54
Matheus Vaula <68791952+matheusvaula@users.noreply.github.com>
2021-07-13 10:09:35 -0300

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -19,3 +19,5 @@ png("plot2.png", width=480, height=480)
 plot(x = powerDT[, dateTime]
      , y = powerDT[, Global_active_power]
      , type="l", xlab="", ylab="Global Active Power (kilowatts)")
+
+dev.off()
