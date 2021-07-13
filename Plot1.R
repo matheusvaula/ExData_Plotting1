d2bc94ed2a7d818ef41e9a39f8a6e112e3f01af3
Matheus Vaula <68791952+matheusvaula@users.noreply.github.com>
2021-07-13 10:09:20 -0300

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -19,3 +19,5 @@ png("plot1.png", width=480, height=480)
 ## Plot 1
 hist(powerDT[, Global_active_power], main="Global Active Power", 
      xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+dev.off()
